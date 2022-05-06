4ca0091bbea11d89bd1cb0becf1ca2a11389bbe1
Ryan <1072458853@qq.com>
2022-05-06 11:35:38 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './style.css'
+
+import Interceptgraph from './Components/interceptgraph.jsx'
+
+class App extends Component {
+  render(){
+    return(
+        <Interceptgraph/>
+    )
+  }
+}
+
+export default App;
